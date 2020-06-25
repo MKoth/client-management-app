f5fbcc19127e364d7a89e93b1823ad4cfc078bed
MKoth <lecorbeaulibre@gmail.com>
2020-06-25 17:11:58 +0300

    Added media editing page for company section

--- src/app/company/media/media.component.ts
+++ src/app/company/media/media.component.ts
@@ -9,7 +9,36 @@ export class MediaComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  isImageInfoChanged:boolean = false;
+
+  ownerImage:string = "https://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/people19.png";
+
+  companyLogo:string = "https://www.leadersinsport.com/wp-content/uploads/2019/10/Uber-website-logo.png";
+
+  onOwnerImageUpdate(newImage){
+    this.isImageInfoChanged = true;
+    this.ownerImage = newImage;
   }
+
+  onOwnerImageDelete(){
+    this.isImageInfoChanged = true;
+    this.ownerImage = null;
+  }
+
+  onLogoUpdate(newLogo){
+    this.isImageInfoChanged = true;
+    this.companyLogo = newLogo;
+  }
+
+  onLogoDelete(){
+    this.isImageInfoChanged = true;
+    this.companyLogo = null;
+  }
+
+  saveMedia(){
+    this.isImageInfoChanged = false;
+  }
+
+  ngOnInit(): void {}
 
 }
